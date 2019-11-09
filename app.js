fc5915a7e106423c1c3d16d2132843173b2680c2
Dominic <dd429@njit.edu>
2019-11-09 14:02:54 -0500

    Added get / route for testing

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var express = require('express')
 var app = express()
 var port = process.env.PORT || 4000
 
+app.get("/", function(req, res){
+    res.send("Hello Welcome to webfolio")
+})
+
 app.listen(4000, function(){
     console.log("App running")
 })
